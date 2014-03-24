39b4b5c059816ef93b39614ce81ce63b7ca8d839
Matt Wynne <matt@mattwynne.net>
2014-03-24 14:58:57 +0000

    Green: Add a working implementation of the web API

--- src/web/app.js
+++ src/web/app.js
@@ -1,5 +1,25 @@
 var express = require('express');
 
 module.exports = function (shop) {
-  return express();
+  var app = express();
+
+  app.use(express.json());
+  app.use(express.logger('dev'));
+
+  app.post('/basket/cucumber', function (req, res, next) {
+    var quantity = req.body.quantity;
+    shop.scan(quantity, function (err) {
+      if (err) return next(err);
+      res.send(201);
+    })
+  });
+
+  app.get('/basket', function (req, res, next) {
+    shop.calculateTotal(function (err, total) {
+      if (err) return next(err);
+      res.json({ total: total });
+    })
+  });
+
+  return app;
 };
