e2bedf90ad241222535b9191347a67ea18a1dbf8
Julien Biezemans <jb@jbpros.com>
2014-03-24 13:09:02 +0100

    Red: Implement step definitions

--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -1,18 +1,15 @@
 module.exports = function () {
 
   this.Given(/^a Cucumber costs \$(\d+)$/, function (price, callback) {
-    // express the regexp above with the code you wish you had
-    callback.pending();
+    this.setPrice(parseInt(price), callback);
   });
 
   this.When(/^I buy (\d+) Cucumbers$/, function (quantity, callback) {
-    // express the regexp above with the code you wish you had
-    callback.pending();
+    this.scan(parseInt(quantity), callback);
   });
 
   this.Then(/^the total should be \$(\d+)$/, function (expectedTotal, callback) {
-    // express the regexp above with the code you wish you had
-    callback.pending();
+    this.totalIs(parseInt(expectedTotal), callback);
   });
 
 };
