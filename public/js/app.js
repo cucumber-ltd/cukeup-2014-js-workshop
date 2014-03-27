4ff9c0ef01891aaec667e054d36f8d3f36eb9272
Matt Wynne <matt@mattwynne.net>
2014-03-27 14:44:06 +0000

    Green: Make the angular app call the back-end API

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,34 @@
-function CheckoutController($scope) {
+function CheckoutController($scope, $http) {
+  var backEnd = new BackEnd();
   var cucumbers = { quantity: 1 };
 
   $scope.cucumbers = cucumbers;
-  $scope.total = 0;
+
+  displayTotal();
 
   $scope.scanCucumber = function () {
-    throw new Error('todo');
+    backEnd.scanCucumber(displayTotal);
   };
+
+  function displayTotal() {
+    backEnd.getBasket(function (basket) {
+      $scope.total = basket.total;
+    });
+  }
+
+  function BackEnd() {
+    var self = {
+      getBasket: function (callback) {
+        $http.get('/basket')
+        .success(callback);
+      },
+
+      scanCucumber: function (callback) {
+        $http.post('/basket/cucumber', { quantity: cucumbers.quantity })
+        .success(callback);
+      }
+    };
+
+    return self;
+  }
 }
